0fbfe7edaa3e546e1084b57228f6dbac2ef1d47a
Devanand Verma <devanandverma01d@gmail.come>
2023-01-03 00:59:06 +0530

    new

--- js/script1.js
+++ js/script1.js
@@ -1,1 +1,8 @@
-`
+
+var student1 = {   // this is an object
+    //1. Property:Value
+    name: "Devanand",  // Property: Value
+    surname: 'Verma'   //property:Value
+    //2.
+}
+console.log("Hello " + student1.name);
---
+++ js/script2.js
@@ -0,0 +1,16 @@
+//1.var 1995 to 2023
+
+//2. let from 2015 under ES6
+
+//3. const from 2015 under ES6
+
+
+const student4 = {  //this is an object
+    //1. Property:value
+    name: "Brendan",
+    surname: "Eich"
+    //2.
+}
+//lets see the output of the js code
+//this is one of the 4 methods to log/display the JS code
+document.write('<h1>' + student4.name + " " + student4.surname + '</h1>');
